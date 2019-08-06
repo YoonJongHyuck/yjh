5d901a1bb2627d68745f4d8dd5a60f34bc3986c2
Administrator <Administrator@WIN10PRO64-36>
2019-08-06 14:07:55 +0900

    ok

--- ease_common/src/main/java/com/ease/common/EaseCommonApplication.java
+++ ease_common/src/main/java/com/ease/common/EaseCommonApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
@@ -11,6 +16,21 @@ public class EaseCommonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EaseCommonApplication.class, args);
+	}
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		templateResolver.setTemplateMode("XHTML");
+		templateResolver.setPrefix("templates/");
+		templateResolver.setSuffix(".html");
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver);
+		
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(engine);
+		
+		return viewResolver;		
 	}
 
 }
